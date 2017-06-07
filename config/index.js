d3354905ee52476bfa76baae6e66e9ba93aaa6bc
Patrick Akpala <kemaakpala@gmail.com>
2017-06-07 14:31:03 +0100

    test port

--- config/index.js
+++ config/index.js
@@ -6,7 +6,6 @@ var environmentVariables = {
   getDbConnectionString : function(){
     //this requires the relevant json environment needed to connect to te database. it still needs some work!!!.
     var envJson = require('./'+ this.env);
-    console.log(this.port);
     return 'mongodb://'+envJson.user+':'+envJson.pwd+'@'+envJson.ip+'/'+envJson.db;
   }
 };
