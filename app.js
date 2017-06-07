20e81359861f54ebe33e0b05b7f0766c00a0da2c
Patrick Akpala <kemaakpala@gmail.com>
2017-06-07 14:41:40 +0100

    test port

--- app.js
+++ app.js
@@ -5,7 +5,6 @@ var mongoose = require('mongoose');
 var config = require('./config');
 var setupController = require('./controllers/setupController');
 var apiController = require('./controllers/apiController');
-var Port = process.env.PORT || 3000; //set default Port value
 
 
 // respond with "hello world" when a GET request is made to the homepage
@@ -22,4 +21,4 @@ setupController(app);
 apiController(app);
 
 
-app.listen(Port);
+app.listen(config.port);
--- config/index.js
+++ config/index.js
@@ -2,7 +2,7 @@
 
 var environmentVariables = {
   env: process.env.NODE_ENV || 'development', //set default environment
-  port: process.env.Port || 3000, //set default Port value
+  port: process.env.PORT | 3000, //set default Port value
   getDbConnectionString : function(){
     //this requires the relevant json environment needed to connect to te database. it still needs some work!!!.
     var envJson = require('./'+ this.env);
