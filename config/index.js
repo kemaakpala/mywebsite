0885ce7987dba9c9140dd97b8e2ab4a8c1be5705
Patrick Akpala <kemaakpala@gmail.com>
2018-01-03 16:00:32 +0000

    app crash error ./production not found

--- config/index.js
+++ config/index.js
@@ -5,29 +5,32 @@ var environmentVariables = {
   port: process.env.PORT | 3000, //set default Port value
   getDbConnectionString : function(){
     //this requires the relevant json environment needed to connect to te database. it still needs some work!!!.
-    var envJson = require('./' + this.env);
+    // var envJson = require('./' + this.env);
     if (this.env !== 'development') {
-      envJson = {
+      var envJson = {
         "user": process.env.DB_USER,
         "pwd": process.env.DB_PASSWORD,
         "ip": process.env.DB_IP,
         "db": process.env.DB_NAME
       };
+    }else {
+      var envJson = require('./' + this.env);
     }
     return 'mongodb://' + envJson.user + ':' + envJson.pwd + '@' + envJson.ip + '/' + envJson.db;
   },
   getMailConnectionObj: function(){
     //this requires the relevant json environment needed to connect to te database. it still needs some work!!!.
-    var envJson = require('./'+ this.env);
     if (this.env !== 'development') {
-      envJson = { 
+      var envJson = { 
         "email": {
           "user": process.env.EMAIL_ADMIN,
           "pwd": process.env.EMAIL_PASSWORD
         }
       };
 
-    } 
+    } else {
+      var envJson = require('./' + this.env);
+    }
     return email = {
       user: envJson.email.user,
       pwd: envJson.email.pwd
